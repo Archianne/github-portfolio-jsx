bf5aaa1a02499febc43a94c0c2cd46a794bce658
Archianne <helena19w@gmail.com>
2021-07-27 21:21:13 +0100

    .

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 import Home from "../../pages/home";
 import Repositories from "../../pages/repositories";
