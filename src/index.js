32dd8b3a5a02a5f8276647fe2802a8e1820c1f49
Archianne <helena19w@gmail.com>
2021-07-27 21:16:12 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 const root = document.getElementById("root");
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  root
+);
